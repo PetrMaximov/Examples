d251b8dd8d32c35bb9c49f01de0373d19d78535a
«Petr» <maxpetrb@mail.ru>
2023-09-03 21:57:48 +0300

    Example 006 Weight

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double numberB = Convert.ToDouble(Console.ReadLine());
+double div = numberA / numberB;
+Console.Write("Частное от деления равно = ");
+Console.Write(div);
