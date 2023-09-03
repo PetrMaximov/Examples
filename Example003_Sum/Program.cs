f6a0323905f8ec18c2f74cad24aa8686e0794ffc
«Petr» <maxpetrb@mail.ru>
2023-09-03 21:12:23 +0300

    Example 003 Summ

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sum = numberA + numberB;
+Console.Write("Сумма равна = ");
+Console.Write(sum);
